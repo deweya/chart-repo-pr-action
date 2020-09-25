1de0f3f185a0a8e91b0db3f2ea311f32df2d1e52
Austin Dewey <deweya964@gmail.com>
2020-09-24 23:11:44 -0500

    creating PR to target. Now need to better parameterize

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,15 +8,21 @@ if [ ! -d "charts" ]; then
   exit 1
 fi
 
-## Clone fork and move local charts over
+## Clone fork
 git clone https://deweya:$INPUT_TOKEN@github.com/deweya/helm-charts
-mv charts/* helm-charts/charts/
-ls -l helm-charts/charts/
 
 ## Push to fork
+## TODO: The pipeline should not fail if no changes are pushed. It should just exit 0 here and not continue to create the PR
 cd helm-charts
 git config user.name $GITHUB_ACTOR
 git config user.email deweya964@gmail.com
+git checkout feat/sync || git checkout -b feat/sync
+cp -r ../charts/* charts/
 git add --all
-git commit -m 'test'
-git push origin master
+git commit -m 'Syncing local charts with Helm chart repo'
+git push origin feat/sync
+
+## Create PR
+export GITHUB_USER=$GITHUB_ACTOR
+export GITHUB_TOKEN=$INPUT_TOKEN
+hub pull-request -b deweya0:master -h feat/sync --no-edit
