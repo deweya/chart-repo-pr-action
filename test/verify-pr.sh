f019b9300d4680f5d82077b78c04e33b350d5809
Austin Dewey <deweya964@gmail.com>
2020-10-01 20:57:10 -0500

    adding test for removing a file to existing chart

--- test/verify-pr.sh
+++ test/verify-pr.sh
@@ -6,13 +6,14 @@ export BASE=$1
 export UPSTREAM_REPO=$2
 export HEAD=$3
 export GITHUB_TOKEN=$4
+export DIFF_FILE=$5
 
 pr_number=$(gh pr list --state open --base $BASE --repo $UPSTREAM_REPO | grep $HEAD | awk '{print $1}')
 gh pr diff $pr_number --repo $UPSTREAM_REPO > pr-diff.txt
 
 set +e
 
-diff pr-diff.txt test/01-pr-diff.txt
+diff pr-diff.txt test/$5
 exit_code=$?
 set -e
 if [ $exit_code -eq 0 ]; then
